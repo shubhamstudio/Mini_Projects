915b77a8442e074a6fffd5354a5937caee69e9c9
shubhamstudio <contact.shubhambisht@gmail.com>
2026-07-18 22:55:16 +0530

    learn EJS templating engine and advanced routing architecture implementation

--- Insta_Demo/index.js
+++ Insta_Demo/index.js
@@ -3,20 +3,26 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
+app.use(express.static(path.join(__dirname, "/public")));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, "/views"));
 
 app.get("/ig/:username", (req, res) => {
-    const followers = ["priyanshu", "ayush", "sujal"]
     let { username } = req.params;
-    console.log(username, followers);
-    res.render('instagram.ejs', { username, followers })
+    const instaData = require("./data.json");
+    const data = instaData[username];
+    if(data){
+        res.render('instagram.ejs', { data: instaData[username] })
+    } else{
+        res.render('error.ejs');
+    }
+
 })
 
 app.get("/", (req, res) => {
     res.send('This page is in maintainence')
 })
-
+ 
 app.listen(port, () => {
     console.log(`app is listening to port ${port}`);
 })
