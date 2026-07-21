beb689582cf2c41253cbaa78fe8a878963ebeb4f
shubhamstudio <contact.shubhambisht@gmail.com>
2026-07-21 12:53:59 +0530

    implement CRUD Rest API routes

--- Rest_Class/index.js
+++ Rest_Class/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
-const {v4: uuidv4} = require('uuid');
+const { v4: uuidv4 } = require('uuid');
+const methodOverride = require("method-override")
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -56,8 +58,21 @@ app.get("/", (req, res) => {
 app.get("/posts/:id", (req, res) => {
     let { id } = req.params;
     let post = posts.find((p) => id === p.id);
-    res.render("show.ejs", {post});
+    res.render("show.ejs", { post });
 });
 
+app.patch("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p) => id === p.id);
+    post.content = newContent;
+    console.log(post);
+    res.redirect("/posts");
+});
 
+app.delete("/posts/:id/", (req, res) => {
+    let { id } = req.params;
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect("/posts");
+});
 
